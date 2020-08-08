51328156369e1a69454fd824f20a33b3fc0c7a0a
danieladov <danielarezdiaz@gmail.com>
2020-08-08 02:34:59 +0200

    Add Split all movies feature
    Now you can split all merged movies

---
+++ Configuration/PluginConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.MergeVersions.Configuration
+{
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+        public int MinimumNumberOfMovies { get; set; }
+
+        public PluginConfiguration()
+        {
+            MinimumNumberOfMovies = 2;
+        }
+    }
+}
